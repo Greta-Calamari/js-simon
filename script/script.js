79ad59c6adc2b24f1c6f52b80ebbd9c76d548258
ladykakki <greta_calamari@hotmail.it>
2022-04-06 14:40:08 +0200

    ...

--- script/script.js
+++ script/script.js
@@ -1,3 +1,26 @@
 // Visualizzare in pagina 5 numeri casuali (in un range tra 1 e 100). Da lì parte un timer di 30 secondi.
 // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+// utility
+ function getRandomInt(min, max) { 
+     min = Math.ceil(min);
+     max = Math.floor(max);
+     return Math.floor(Math.random() * (max - min) + min);
+     }
+     
+    //  funzione che genera 5 numeri unici e li mette nell'array
+     
+     let numeri = [];
+     let testo = '';
+     
+     while(numeri.length < 5){
+         let random = Math.floor(Math.random() * 100) + 1;
+         if(numeri.indexOf(random) === -1) numeri.push(random);
+     }
+     console.log(numeri);
+     
+
+    
+    
+     
